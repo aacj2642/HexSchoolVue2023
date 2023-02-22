9eb15b15b290b2411e24f7efebf104400edfd024
aacj2642 <aacj2642@gmail.com>
2023-02-22 23:56:43 +0800

    助教建議(dist)

---
+++ week7/dist/assets/IndexView-ab2d9248.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-071cb391.js";const n={name:"IndexView"},o={class:"text-center"};function s(r,a,_,p,d,i){return t(),c("h2",o,"首頁")}const x=e(n,[["render",s]]);export{x as default};
